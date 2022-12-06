319ff5ce5415338979654b3ca2088f474cbc6013
Cameron Gould <cameron@gouldcs.com>
2022-12-05 21:13:51 -0800

    Just a website.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Just a website
+
+`npm run start` to run
+
+If your code builds successfully, I'll merge the PR.
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nothing to see here</h1>
+      <p>This is just a website.</p>
+    </div>
+  );
+}
+
+export default App;
